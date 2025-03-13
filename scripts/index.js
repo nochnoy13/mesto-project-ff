35fd9d8e7572953a3bb5e3fbfa293461f53c29de
Aleksey Naidenov-Kazantsev <nochnoy13@yandex.ru>
2025-03-14 01:16:31 +0300

    первое исправление

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,40 @@
 
 // @todo: Вывести карточки на страницу
 
-const template = document.querySelector('#card-template');
+// const template = document.querySelector('#card-template').content;
+// const placeList = document.querySelector('.places__list');
+
+// function createCard(card, deleteCallback) {
+//     const newCard = template.querySelector('.card').cloneNode(true);
+    
+//     const cardImage = newCard.querySelector('.card__image');
+//     cardImage.src = card.link;
+//     cardImage.alt = card.name;
+
+//     const cardTitle = newCard.querySelector('.card__title');
+//     cardTitle.textContent = card.name;
+
+//     const deleteButton = newCard.querySelector('.card__delete-button');
+//     deleteButton.addEventListener('click', handleDeleteCard);
+
+//     return newCard
+// }
+
+// function handleDeleteCard(event) {
+//     event.target.closest('.card').remove();
+// }
+
+// initialCards.forEach(card => {
+//     const newCard = createCard(card);
+//     placeList.append(newCard);
+// });
+
+
+const template = document.querySelector('#card-template').content;
 const placeList = document.querySelector('.places__list');
 
-function createCard(card) {
-    const newCard = template.content.cloneNode(true);
+function createCard(card, deleteCallback) {
+    const newCard = template.querySelector('.card').cloneNode(true);
     
     const cardImage = newCard.querySelector('.card__image');
     cardImage.src = card.link;
@@ -21,18 +50,18 @@ function createCard(card) {
     const cardTitle = newCard.querySelector('.card__title');
     cardTitle.textContent = card.name;
 
-    const deleteButton = newCard.querySelector(".card__delete-button");
-    deleteButton.addEventListener("click", handleDeleteCard);
+    const deleteButton = newCard.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCallback(newCard));
     
+    return newCard;
+}
+
+function handleDeleteCard(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach(card => {
+    const newCard = createCard(card, handleDeleteCard);
     placeList.append(newCard);
-
-    return newCard
-}
-
-function handleDeleteCard(event) {
-    event.target.closest(".card").remove();
-}
-
-initialCards.forEach(createCard);
-
+});
 
